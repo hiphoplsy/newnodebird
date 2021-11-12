27c52966b2b35489af29963bb7e345ef7901de97
hiphoplsy <hiphoplsy@naver.com>
2021-11-12 15:12:29 +0900

    ADD: Backend Database & nodemon

--- back/models/comment.js
+++ back/models/comment.js
@@ -9,6 +9,9 @@ module.exports = (sequelize, DataTypes) => {
     charset: 'utf8mb4',
     collate: 'utf8mb4_general_ci', //한글 저장 mb4 -> 이모티콘 저장
   });
-  Comment.associate = (db) => {};
+  Comment.associate = (db) => {
+    db.Comment.belongsTo(db.User);
+    db.Comment.belongsTo(db.Post);
+  };
   return Comment;
 };
--- back/models/post.js
+++ back/models/post.js
@@ -9,6 +9,13 @@ module.exports = (sequelize, DataTypes) => {
     charset: 'utf8mb4',
     collate: 'utf8mb4_general_ci', //한글 저장, mb4 -> 이모티콘저장
   });
-  Post.associate = (db) => {};
+  Post.associate = (db) => {
+    db.Post.belongsTo(db.User);
+    db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' });
+    db.Post.hasMany(db.Comment);
+    db.Post.hasMany(db.Image);
+    db.Post.belongsToMany(db.User, { through: 'Like', as: 'Liked' });
+    db.Post.belongsTo(db.Post, { as: 'Retweet' });
+  };
   return Post;
 };
