4704e11a3eb4d4b3687ff69f87b880b52e862fba
hiphoplsy <hiphoplsy@naver.com>
2021-11-13 10:45:30 +0900

    ADD: passport & login

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const passport = require('passport');
 const { User } = require('../models');
 
 const router = express.Router();
+
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      console.error(err);
+      next(err);
+    }
+    if (info) {
+      return res.status(403).send(info.reason);
+    }
+    return req.login(user, async (loginErr) => {
+      if (loginErr) {
+        console.error(loginErr);
+        return next(loginErr);
+      }
+      return res.json(user);
+    });
+  })(req, res, next);
+});
 
 router.post('/', async (req, res, next) => { // POST /user/
   try {
