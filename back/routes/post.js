854c41f379dc30952bd27c30093af156b506223e
hiphoplsy <hiphoplsy@naver.com>
2021-12-02 17:26:06 +0900

    ADD: Hashtags

--- back/routes/post.js
+++ back/routes/post.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-const { Post, Image, Comment, User } = require('../models');
+const { Post, Image, Comment, User, Hashtag } = require('../models');
 const { isLoggedIn } = require('./middlewares');
 
 const router = express.Router();
@@ -31,10 +31,17 @@ const upload = multer({
 
 router.post('/', isLoggedIn, upload.none(), async (req, res, next) => { // /POST /post
   try {
+    const hashtags = req.body.content.match(/#[^\s#]+/g);
     const post = await Post.create({
       content: req.body.content,
       UserId: req.user.id,
     });
+    if (hashtags) {
+      const result = await Promise.all(hashtags.map((tag) => Hashtag.findOrCreate({
+        where: { name: tag.slice(1).toLowerCase() },
+      }))); // [[노드, true], [리액트, true]] => 배열로 나오게 되므로 아래 map 반복문을 통해 첫번째 항목만 가져올 수 있게 함
+    await post.addHashtags(result.map((v) => v[0]));
+    }
     if (req.body.image) {
       if (Array.isArray(req.body.image)) { // 이미지를 여러개 올리면 [학진쓰1.png, 학진쓰2.png] 배열로 감싸짐
         const images = await Promise.all(req.body.image.map((image) => Image.create({ src: image })));
