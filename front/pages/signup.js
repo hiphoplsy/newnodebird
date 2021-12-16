1bed20d28dc757cb2466079eb6595ee996c4d540
hiphoplsy <hiphoplsy@naver.com>
2021-12-16 16:04:20 +0900

    ADD: getServerSideProps

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,10 +1,13 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button, Checkbox, Form, Input } from 'antd';
+import axios from 'axios';
 import Router from 'next/router';
+import { END } from 'redux-saga';
 
 import useInput from '../hooks/useInput';
-import { SIGN_UP_REQUEST } from '../reducers/user';
+import { LOAD_MY_INFO_REQUEST, SIGN_UP_REQUEST } from '../reducers/user';
+import wrapper from '../store/configureStore';
 
 const signup = () => {
   const dispatch = useDispatch();
@@ -120,5 +123,20 @@ const signup = () => {
     </>
   );
 };
+
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+  console.log('getServerSideProps start');
+  const cookie = context.req ? context.req.headers.cookie : '';
+  axios.defaults.headers.Cookie = '';
+  if (context.req && cookie) {
+    axios.defaults.headers.Cookie = cookie;
+  }
+  context.store.dispatch({
+    type: LOAD_MY_INFO_REQUEST,
+  });
+  context.store.dispatch(END);
+  console.log('getServerSideProps end');
+  await context.store.sagaTask.toPromise();
+});
 
 export default signup;
